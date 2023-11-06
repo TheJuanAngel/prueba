9aae842ff26af80cfbed1ef7cc80a43c7bd487af
TheJuanAngel <150067338+TheJuanAngel@users.noreply.github.com>
2023-11-06 11:41:41 -0600

    Add files via upload

---
+++ prueba.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+    <title>Sabor Fresco</title>
+</head>
+<body>
+    <header>
+        <h1 href="#" class="logo">Sabor fresco</h1>
+    </header>
+    <selection class="menu" id="menu">
+        <div class="titulo">
+            <span>Nuestro Menu</span>
+            <h2>La mejor comida de todo Monterrey</h2>
+        </div>
+        <div class="contenedor-menu">
+            <div class="contenido">
+                <div class="contenido-imagen">
+                    <img src="liluzi.png" alt="comida 1" width="260" height="260px">
+
+                    <h2>Lil uzi vs the world</h2>
+                    <h3>lil uzi</h3>
+                    <h4>$14.99</h4>
+                    <span class="carrito">&#128722;</span>
+                </div>
+            </div>
+            <div class="contenido">
+                <div class="contenido-imagen">
+                    <img src="liluzi.png" alt="comida 2" width="260px" height="260px">
+
+                    <h2>Lil uzi vs the world</h2>
+                    <h3>lil uzi</h3>
+                    <h4>$14.99</h4>
+                    <span class="carrito">&#128722;</span>
+                </div>
+            </div>
+            <div class="contenido">
+                <div class="contenido-imagen">
+                    <img src="liluzi.png" alt="comida 3" width="260px" height="260px">
+
+                    <h2>Lil uzi vs the world</h2>
+                    <h3>lil uzi</h3>
+                    <h4>$14.99</h4>
+                    <span class="carrito">&#128722;</span>
+                </div>
+            </div>
+            <div class="contenido">
+                <div class="contenido-imagen">
+                    <img src="liluzi.png" alt="comida 4" width="260px" height="260px">
+
+                    <h2>Lil uzi vs the world</h2>
+                    <h3>lil uzi</h3>
+                    <h4>$14.99</h4>
+                    <span class="carrito">&#128722;</span>
+                </div>
+            </div>
+            <div class="contenido">
+                <div class="contenido-imagen">
+                    <img src="liluzi.png" alt="comida 5" width="260px" height="260px">
+
+                    <h2>Lil uzi vs the world</h2>
+                    <h3>lil uzi</h3>
+                    <h4>$14.99</h4>
+                    <span class="carrito">&#128722;</span>
+                </div>
+            </div>
+            <div class="contenido">
+                <div class="contenido-imagen">
+                    <img src="liluzi.png" alt="comida 6" width="260px" height="260px">
+
+                    <h2>Lil uzi vs the world</h2>
+                    <h3>lil uzi</h3>
+                    <h4>$14.99</h4>
+                    <span class="carrito">&#128722;</span>
+                </div>
+            </div>
+            <div class="contenido">
+                <div class="contenido-imagen">
+                    <img src="liluzi.png" alt="comida 7" width="260px" height="260px">
+
+                    <h2>Lil uzi vs the world</h2>
+                    <h3>lil uzi</h3>
+                    <h4>$14.99</h4>
+                    <span class="carrito">&#128722;</span>
+                </div>
+            </div>
+            <div class="contenido">
+                <div class="contenido-imagen">
+                    <img src="liluzi.png" alt="comida 8" width="260px" height="260px">
+
+                    <h2>Lil uzi vs the world</h2>
+                    <h3>lil uzi</h3>
+                    <h4>$14.99</h4>
+                    <span class="carrito">&#128722;</span>
+                </div>
+            </div>
+        </div>     
+    </slection>
+
+<?php
+
+
+?>
+</body>
+</html>
